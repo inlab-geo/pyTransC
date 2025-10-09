7f71c0b816e001cba6eb668571696c398e3fafe0
auggiemarignier <auggie.marignier@earth.ox.ac.uk>
2025-10-09 17:08:20 +0100

    refactor: pull out the call to run_mcmc

--- src/pytransc/utils/auto_pseudo.py
+++ src/pytransc/utils/auto_pseudo.py
@@ -192,11 +192,11 @@ pseudo_prior_factories: dict[PseudoPriorOptions, PseudoPriorBuilder] = {
 def build_auto_pseudo_prior(
     pseudo_prior_type: PseudoPriorOptions = PseudoPriorOptions.GAUSSIAN_MIXTURE,
     *,
-    ensemble_per_state: list[FloatArray] = [],
-    log_posterior: MultiStateDensity | None = None,
+    ensemble_per_state: list[FloatArray] | None = None,
+    log_posterior_fn: MultiStateDensity | None = None,
     log_pseudo_prior_fn: MultiStateDensity | None = None,
     draw_deviate_fn: MultiStateDraw | None = None,
-    sampling_args: dict[str, Any] = {},
+    sampling_args: dict[str, Any] | None = None,
     **builder_kwargs,
 ) -> SampleableMultiStateDensity:
     """
@@ -210,8 +210,12 @@ def build_auto_pseudo_prior(
         List of posterior samples for each state. If not provided, samples will be generated using MCMC.
     log_posterior : MultiStateDensity, optional
         Function evaluating the log-posterior for each state. Required if ensemble_per_state is not provided.
+    log_pseudo_prior_fn : MultiStateDensity, optional
+        Custom function to evaluate the log pseudo-prior density. Required if pseudo_prior_type is CUSTOM.
+    draw_deviate_fn : MultiStateDraw, optional
+        Custom function to draw a random deviate from the pseudo-prior. Required if pseudo_prior_type is CUSTOM.
     sampling_args : dict, optional
-        Arguments for MCMC sampling if ensemble_per_state is not provided.
+        Arguments for MCMC sampling if ensemble_per_state is not provided.  Must include 'n_states', 'n_dims', 'n_walkers', 'n_steps', and 'pos'.
     **builder_kwargs : dict
         Additional arguments passed to the pseudo-prior builder.
 
@@ -220,9 +224,17 @@ def build_auto_pseudo_prior(
     log_pseudo_prior : PseudoPrior
         Callable function to evaluate the log pseudo-prior at a given point and state.
     """
-    builder = pseudo_prior_factories.get(pseudo_prior_type)
-    if builder is None:
-        raise InputError(f"Unsupported pseudo_prior_type: {pseudo_prior_type}")
+    try:
+        pseudo_prior_type = PseudoPriorOptions(pseudo_prior_type)
+    except ValueError:
+        raise InputError(f"Invalid pseudo_prior_type: {pseudo_prior_type}")
+
+    builder = pseudo_prior_factories[pseudo_prior_type]
+
+    if ensemble_per_state is None:
+        ensemble_per_state = []
+    if sampling_args is None:
+        sampling_args = {}
 
     if pseudo_prior_type == PseudoPriorOptions.CUSTOM:
         if log_pseudo_prior_fn is None or draw_deviate_fn is None:
@@ -235,34 +247,43 @@ def build_auto_pseudo_prior(
             draw_deviate_fn=draw_deviate_fn,
         )
 
-    if ensemble_per_state is None:  # generate samples for fitting
-        if log_posterior is None:
+    if not ensemble_per_state:
+        if log_posterior_fn is None:
             raise InputError(
                 "log_posterior must be provided if ensemble_per_state is not supplied."
             )
-        if any(
-            k not in sampling_args
-            for k in ["n_states", "n_dims", "n_walkers", "n_steps", "pos"]
-        ):
-            raise InputError(
-                "sampling_args must contain 'n_states', 'n_dims', 'n_walkers', 'n_steps', and 'pos' keys."
-            )
-
-        n_states = sampling_args.pop("n_states")
-        n_dims = sampling_args.pop("n_dims")
-        n_walkers = sampling_args.pop("n_walkers")
-        n_steps = sampling_args.pop("n_steps")
-        pos = sampling_args.pop("pos")
-        ensemble_per_state, _ = run_mcmc_per_state(
-            n_states=n_states,
-            n_dims=n_dims,
-            n_walkers=n_walkers,
-            n_steps=n_steps,
-            pos=pos,
-            log_posterior=log_posterior,
-            **sampling_args,
+        ensemble_per_state = _generate_missing_ensembles(
+            log_posterior=log_posterior_fn, sampling_args=sampling_args
         )
 
     log_pseudo_prior = builder(ensemble_per_state, **builder_kwargs)
 
     return log_pseudo_prior
+
+
+def _generate_missing_ensembles(
+    log_posterior: MultiStateDensity,
+    sampling_args: dict[str, Any],
+) -> list[FloatArray]:
+    if any(
+        k not in sampling_args
+        for k in ["n_states", "n_dims", "n_walkers", "n_steps", "pos"]
+    ):
+        raise InputError(
+            "sampling_args must contain 'n_states', 'n_dims', 'n_walkers', 'n_steps', and 'pos' keys."
+        )
+
+    n_states = sampling_args.pop("n_states")
+    n_dims = sampling_args.pop("n_dims")
+    n_walkers = sampling_args.pop("n_walkers")
+    n_steps = sampling_args.pop("n_steps")
+    pos = sampling_args.pop("pos")
+    return run_mcmc_per_state(
+        n_states=n_states,
+        n_dims=n_dims,
+        n_walkers=n_walkers,
+        n_steps=n_steps,
+        pos=pos,
+        log_posterior=log_posterior,
+        **sampling_args,
+    )[0]  # return only the ensembles
